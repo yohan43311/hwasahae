59603e715ddff71a7cc5b33e079f73afddc081a6
Dahyeon Lee <dada4202@gmail.com>
2023-11-01 18:34:15 +0900

    client initial commit

--- client/app.js
+++ client/app.js
@@ -1,12 +1,16 @@
 // node_modules 에 있는 express 관련 파일을 가져온다.
 var express = require("express");
+const dotenv = require("dotenv");
+dotenv.config({ path: ".env.local" }); //.env.local로 기본 경로 설정
 
 // express 는 함수이므로, 반환값을 변수에 저장한다.
 var app = express();
 
 // 3000 포트로 서버 오픈
-app.listen(3000, function () {
-  console.log("start! express server on port 3000");
+const port = process.env.PORT || 4000;
+
+app.listen(port, function () {
+  console.log(`start! express server on port ${port}`);
 });
 
 app.get("/", function (req, res) {
