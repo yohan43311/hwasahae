ae1f5258743e9e097b4c9206d2b309da19efe2a6
김나영 <newpaper.nana0@gmail.com>
2023-11-01 18:00:31 +0900

    client initial commit

---
+++ client/src/pages/SignIn/signIn.js
@@ -0,0 +1,8 @@
+const login_Btn = document.querySelector("#login-btn");
+const joinUs_Btn = document.querySelector("#joinUs-btn");
+const IdPW_Btn = document.querySelector("#findIdPw-btn");
+
+function signUpLink() {
+  link = "pages/SignUp/signUp.html";
+  window.location.href = link;
+}
