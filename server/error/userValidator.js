63b6d7a50df08516d02ae21cfc638ebb1a8aaac6
Dahyeon Lee <dada4202@gmail.com>
2023-11-06 13:09:06 +0900

    Feat: userValidator 유효성검사 오류 반환을 공통형식으로 변경

--- server/error/userValidator.js
+++ server/error/userValidator.js
@@ -11,7 +11,9 @@ const validate = (req, res, next) => {
 
   //유효성 검사를 통과하지 못했다면
   //오류 반환
-  return res.status(400).json({ message: errors.array()[0] });
+  const error = new Error(`${errors.array()[0]?.msg}`);
+  error.status = 400;
+  throw error;
 };
 
 //유저 회원가입 유효성 검사
