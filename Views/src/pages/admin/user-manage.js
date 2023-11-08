7795b34aca4d1d5f9e30e3b59e0018b291f68e68
vision <visionmdl520@gmail.com>
2023-11-08 16:22:41 +0900

    design

---
+++ Views/src/pages/admin/user-manage.js
@@ -0,0 +1,50 @@
+//회원목록 테이블
+const user_table = document.querySelector('.user_table')
+
+window.onload =function (){
+  console.log('회원관리 페이지')
+}
+
+
+var requestOptions = {
+  method: 'GET',
+  redirect: 'follow'
+};
+
+fetch("http://localhost:3000/users", requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => {
+    console.log('error가 발생했어요!', error)
+    user_table.innerHTML = createUserList(test_user_data)
+  });
+
+  const createUserList = (data)=>{
+    return data.reduce((prev,cur)=> prev+ `
+    <tr>
+                      <th scope="row">${cur['id']}</th>
+                      <td>${cur['name']}</td>
+                      <td>${cur['email']}</td>
+                      <td>${cur['zipcode']}</td>
+                      <td><button type="button" class="btn btn-light rounded-pill"
+                        onclick="location.href='user-modify.html' "
+                        >수정</button></td>
+                    </tr>
+    `,'')
+  }
+
+  //test 유저데이터
+const test_user_data = [
+  {
+    index : 2,
+    name : '실패 천비전',
+    email : 'ess@naver.com',
+    zipcode : '12121',
+  },
+  {
+    index : 3,
+    name : '실패 호호홍',
+    email : 'mdid@naver.com',
+    zipcode : '44444',
+  }
+]
