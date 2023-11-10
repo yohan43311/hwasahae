1a8ad233b2466f994ad3c582050548463d0a1b85
yohan <johan43311@gmail.com>
2023-11-10 13:55:35 +0900

    Feat: 관리자 주문 조회

--- Controllers/orderController.js
+++ Controllers/orderController.js
@@ -5,7 +5,14 @@ const orderService = new OrderService();
 
 // 주문 추가 (유저)
 const createOrder = asyncHandler(async (req, res) => {
-  const populatedOrder = await orderService.createOrder(req.body);
+  //인증된 사용자의 _id를 `userId`로 설정합니다.
+  const userId = res.locals.userInfo._id; // 금 id -> _id
+  const orderData = {
+    ...req.body,
+    userId, // 인증된 사용자의 _id를 주문의 userId로 설정합니다.
+  };
+  const populatedOrder = await orderService.createOrder(orderData);
+  // const populatedOrder = await orderService.createOrder(req.body);
   res.status(200).json(populatedOrder);
 });
 // 주문 조회 (유저)
--- Views/src/pages/admin/order-modify.js
+++ Views/src/pages/admin/order-modify.js
@@ -1,34 +1,28 @@
+window.onload = function () {
+  const receivedData = location.href.split("?")[1];
+  console.log("주문 수정 페이지", typeof receivedData, receivedData);
+  let params = receivedData.split("&");
+  console.log("파람^^ ", params);
+  const requestParameter = params.reduce((prev, cur) => {
+    const v = cur.split("=");
+    console.log("브이 ", v, v[0], v[1]);
+    prev[v[0]] = v[1];
+    return prev;
+  }, {});
+  console.log(
+    "주문 관리 페이지로 부터 받아온 수정할 데이터 (백엔드로 넘겨줘야할 데이터) : ",
+    typeof requestParameter,
+    requestParameter
+  );
 
+  // 1. fetch 해서 수정 할 주문 정보 가져오기!
 
+  // input 태그 안에 value로 매핑
+};
 
-
-window.onload =function (){
-    const receivedData = location.href.split('?')[1];
-    console.log('주문 수정 페이지',typeof receivedData,receivedData)
-    let params = receivedData.split('&')
-    console.log('파람^^ ',params)
-    const requestParameter =  params.reduce((prev,cur)=>{
-        const v = cur.split('=')
-        console.log('브이 ',v,v[0],v[1])
-        prev[v[0]] = v[1]
-        return prev
-    }, {})
-    console.log('주문 관리 페이지로 부터 받아온 수정할 데이터 (백엔드로 넘겨줘야할 데이터) : ', typeof requestParameter,requestParameter)
-
-    // 1. fetch 해서 수정 할 주문 정보 가져오기!
-    
-
-
-    // input 태그 안에 value로 매핑
-
-}
-
-
-const moditybutton = document.querySelector('.modify_button')
-const deletebutton = document.querySelector('.delete_button')
+const moditybutton = document.querySelector(".modify_button");
+const deletebutton = document.querySelector(".delete_button");
 
 // 매핑된 정보를 수정 완료하기 기능
-// 현재 input 태그안에 있는 value를 백엔드로 보내는 fetch 작성 
-const postModifiedData = ()=>{
-
-}
+// 현재 input 태그안에 있는 value를 백엔드로 보내는 fetch 작성
+const postModifiedData = () => {};
