333d4fbbdb33597d3433f9e0317b67746ce1329a
harime <zing10@naver.com>
2023-11-11 17:48:59 +0900

    fix

--- Views/src/pages/Product/product.js
+++ Views/src/pages/Product/product.js
@@ -1,9 +1,12 @@
 
 const categoryNav = document.querySelector('.list')
+
 let linkName = []
 // 카테고리 데이터로 불러와서 만들기.
 async function fetchData() {
   try {
+
+    const storedCart = JSON.parse(window.localStorage.getItem("cart") || "[]"); // 가져오기
     const response = await fetch('http://localhost:3000/category');
     const data = await response.json();
     console.log(data);
@@ -12,25 +15,64 @@ async function fetchData() {
       <li><a href="" class="linkName" data-name="${item.name}">${item.name}</a></li>
     `).join('');
     categoryNav.innerHTML = category_item_nav;
-
     const linkBtns = document.querySelectorAll('.linkName');
 
     linkBtns.forEach(linkBtn => {
       linkBtn.addEventListener('click', (e) => {
         e.preventDefault();
         const dataNameValue = linkBtn.getAttribute('data-name');
-        alert(dataNameValue);
+ 
         linkName = []
         linkName.push(dataNameValue);
 
         listHtml(linkName)
+        cartB()
       });
     });
+    const cartB = () =>{
+      setTimeout(()=>{
+        const cartBtns = document.querySelectorAll(".cartDateBtnMd"); //버튼클래스
+        const cartList = storedCart; //장바구니 리스트
+        cartBtns.forEach((cartBtn) => {
+          const dataValue = cartBtn.dataset.value; //상품의 아이디값
+          const dataPrice = cartBtn.getAttribute("data-another"); //상품의 가격 데이터
+        
+          cartBtn.addEventListener("click", (e) => {
+            if (cartList.some((item) => item.id === dataValue)) {
+              //장바구니 스토리지 중복확인.
+              alert("이미 장바구니에 있습니다.");
+              return;
+            } else {
+              cartList.push({
+                id: dataValue,
+                price: dataPrice,
+                count: 1,
+              }); //상품 아이디를 배열에 넣음
+              window.localStorage.setItem("cart", JSON.stringify(cartList)); // 스토리지에 장바구니 아이템넣음.
+              alert("장바구니에 넣었습니다.");
+            }
+          });
+        });
+      
+      },100)
+    }
+    cartB()
+    
+
+
   } catch (error) {
     console.error('Error fetching data:', error);
   }
+
 }
-fetchData();
+fetchData().then(() => {
+  // fetchData가 완료된 후 listHtml 호출
+  listHtml(linkName);
+  
+});
+
+
+
 
 // const koreanText = '스킨'; // 여기에 실제로 사용하려는 한글 텍스트를 넣어주세요
 // fetch(`http://localhost:3000/category/products/${koreanText}`).then((response) => response.json()).then((data)=>console.log(data))
@@ -57,6 +99,9 @@ const listHtml = async (link) => {
               <a href="/item?id=${id}">
                 <img src="${imageSrc}" alt="상품 섬네일">
               </a>
+              <div class="info_icon">
+                <span class="cartDateBtnMd" data-value="${id}" data-another="${price}"><img src="http://skincure.co.kr/design/skincure/0759ansome/icon_prd04.gif" alt="미리보기"></span>
+              </div>
             </div>
             <div class="product-info">
               <div class="product-title">${title}</div>
@@ -66,6 +111,8 @@ const listHtml = async (link) => {
             </div>
           </div>
         `;
+
+        
     return productTemplate;
   }
   // 동적 상품 추가
@@ -78,7 +125,6 @@ const listHtml = async (link) => {
   });
 };
 
-// 예시 사용법:
 listHtml();
 
 
